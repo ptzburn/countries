c16bd2656ace3e351d41d2db85ac632ffeafdf3d
ptzburn <milan.hokkanen@gmail.com>
2025-05-10 19:52:55 +0300

    first complete attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
-const App = () => {}
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Main from './components/Main'
+
+import HomePage from './pages/HomePage'
+import Details from './pages/Details'
+import NotFound from './pages/NotFound'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  return (
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <HomePage countries={countries} setCountries={setCountries} />
+            }
+          />
+          <Route path="/country/:name" element={<Details />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Main>
+    </>
+  )
+}
 
 export default App
---
+++ src/pages/Details.jsx
@@ -0,0 +1,30 @@
+import { useParams, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { IoArrowBack } from 'react-icons/io5'
+import { searchByCountry } from '../config'
+import { Button } from '../components/Button'
+import Info from '../components/Info'
+
+const Details = () => {
+  const { name } = useParams()
+  const navigate = useNavigate()
+  const [country, setCountry] = useState(null)
+
+  console.log(country)
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]))
+  }, [name])
+
+  return (
+    <div>
+      <Button onClick={() => navigate('/')}>
+        <IoArrowBack /> Back
+      </Button>
+      {country && <Info {...country} />}
+    </div>
+  )
+}
+
+export default Details
